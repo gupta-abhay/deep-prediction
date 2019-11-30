a572d282c82ec4f03aabf656ba1a29f4e655acd8
Abhay Gupta <gupta94abhay@gmail.com>
2019-11-30 18:37:48 -0500

    adding modified training code

--- train_modified.py
+++ train_modified.py
@@ -5,6 +5,8 @@ from argoverse.visualization.visualize_sequences import viz_sequence
 from statistics import mean
 import glob
 import torch
+import pandas as pd
+from argoverse.visualization.visualize_sequences import viz_sequence
 # torch.multiprocessing.set_sharing_strategy('file_system')
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -22,10 +24,12 @@ import warnings
 from time import localtime, strftime
 # from logtger import TensorLogger
 import numpy as np
+from visualize import viz_predictions
 import os
 import threading
 import copy
 import resource
+import pdb
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
 resource.setrlimit(resource.RLIMIT_NOFILE, (4096, rlimit[1]))
 
@@ -61,12 +65,7 @@ class Trainer():
 
         self.clip = clip
         self.max_grad_norm = max_grad_norm
-        # self.model_dir="./models/LSTM/20191104171311/"
-        # self.test_path="./test/LSTM/20191104171311/"
-    # def inverse_transform_rotation_translation(self,trajectory,R,t):
-    #     out=torch.matmul(R.permute(0,2,1),trajectory.permute(0,2,1)).permute(0,2,1)
-    #     out=out-t.reshape(t.shape[0],1,2)
-    #     return out
+
     def check_normalization(self):
         for i_batch,traj_dict in enumerate(self.val_loader):
             pred_traj=traj_dict['gt_agent']
@@ -84,6 +83,10 @@ class Trainer():
         self.model.train()
         no_samples=0
         for i_batch,traj_dict in enumerate(self.train_loader):
+            gt_traj=traj_dict['gt_agent']
+            if self.use_cuda:
+                gt_traj=gt_traj.cuda()
+            
             if self.model_type == 'VRAE':
                 pred_traj, latent_traj, latent_mean, latent_logvar = self.model(traj_dict)
                 kl_loss = -0.5 * torch.mean(1 + latent_logvar - latent_mean.pow(2) - latent_logvar.exp())
@@ -91,15 +94,8 @@ class Trainer():
                 loss = kl_loss + mse_loss
             else:
                 pred_traj=self.model(traj_dict)
-                gt_traj=traj_dict['gt_agent']
-                if self.use_cuda:
-                    gt_traj=gt_traj.cuda()
                 loss=self.loss_fn(pred_traj,gt_traj)
 
-            # gt_traj=traj_dict['gt_agent']
-            # if self.use_cuda:
-            #     gt_traj=gt_traj.cuda()
-            # loss=self.loss_fn(pred_traj,gt_traj)
             total_loss=total_loss+loss.data
             avg_loss = float(total_loss)/(i_batch+1)
             print(f"Training Iter {i_batch+1}/{num_batches} Avg Loss {avg_loss:.4f}",end="\r")
@@ -108,7 +104,7 @@ class Trainer():
             if self.clip:
                 torch.nn.utils.clip_grad_norm(self.model.parameters(), max_norm = self.max_grad_norm)
             self.optimizer.step()
-        print()
+        
         return total_loss/(num_batches)
 
     def val_epoch(self, epoch):
@@ -121,31 +117,21 @@ class Trainer():
         no_samples=0
         
         for i_batch,traj_dict in enumerate(self.val_loader):
+            gt_traj = traj_dict['gt_unnorm_agent']
+            if self.use_cuda:
+                gt_traj=gt_traj.cuda()
+            
             if self.model_type == 'VRAE':
                 pred_traj, latent_traj, latent_mean, latent_logvar = self.model(traj_dict)
                 pred_traj = self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-                gt_traj = traj_dict['gt_unnorm_agent']
-                if self.use_cuda:
-                    gt_traj=gt_traj.cuda()
-
                 kl_loss = -0.5 * torch.mean(1 + latent_logvar - latent_mean.pow(2) - latent_logvar.exp())
                 mse_loss=self.loss_fn(pred_traj,gt_traj)
                 loss = kl_loss + mse_loss
             else:
                 pred_traj=self.model(traj_dict)
                 pred_traj=self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-                gt_traj=traj_dict['gt_unnorm_agent']
-                if self.use_cuda:
-                    gt_traj=gt_traj.cuda()
                 loss=self.loss_fn(pred_traj,gt_traj)
 
-            # pred_traj=self.model(traj_dict)
-            # pred_traj=self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-            # gt_traj=traj_dict['gt_unnorm_agent']
-            # if self.use_cuda:
-            #     gt_traj=gt_traj.cuda()
-                
-            # loss=self.loss_fn(pred_traj,gt_traj)
             total_loss=total_loss+loss.data
             batch_samples=gt_traj.shape[0]           
             
@@ -160,12 +146,6 @@ class Trainer():
             fde_one_sec_avg = float(fde_one_sec)/no_samples
             fde_three_sec_avg = float(fde_three_sec)/no_samples
 
-            # self.writer.scalar_summary('Val/AvgLoss', float(total_loss)/(i_batch+1), i_batch+1)
-            # self.writer.scalar_summary('Val/1ADE', ade_one_sec_avg, i_batch+1)
-            # self.writer.scalar_summary('Val/3ADE', ade_three_sec_avg, i_batch+1)
-            # self.writer.scalar_summary('Val/1FDE', fde_one_sec_avg, i_batch+1)
-            # self.writer.scalar_summary('Val/3FDE', fde_three_sec_avg, i_batch+1)
-
             # if (i_batch+1) % self.args.val_log_interval == 0:
             print(f"Validation Iter {i_batch+1}/{num_batches} Avg Loss {total_loss/(i_batch+1):.4f} \
             One sec:- ADE:{ade_one_sec/(no_samples):.4f} FDE: {fde_one_sec/(no_samples):.4f}\
@@ -173,7 +153,6 @@ class Trainer():
 
             _filename = self.model_dir + 'best-model.pt'
 
-            # if ade_one_sec_avg < self.best_1_ade and ade_three_sec_avg < self.best_3_ade and fde_one_sec_avg < self.best_1_fde and fde_three_sec_avg < self.best_3_fde:
             if ade_three_sec_avg < self.best_3_ade and fde_three_sec_avg < self.best_3_fde:    
                 torch.save({
                     'epoch': epoch,
@@ -218,7 +197,6 @@ class Trainer():
         print("done")
 
     def validate_model(self, model_path):
-        self.save_top_errors_accuracy(model_dir)
         total_loss=0
         num_batches=len(self.val_loader.batch_sampler)
         self.model.load_state_dict(torch.load(model_path+'best-model.pt')['model_state_dict'])
@@ -228,25 +206,22 @@ class Trainer():
         no_samples=0
         
         for i_batch,traj_dict in enumerate(self.val_loader):
+            gt_traj=traj_dict['gt_unnorm_agent']
+            if self.use_cuda:
+                gt_traj=gt_traj.cuda()
+            
             if self.model_type == 'VRAE':
                 pred_traj, latent_traj, latent_mean, latent_logvar = self.model(traj_dict)
                 pred_traj = self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-                gt_traj = traj_dict['gt_unnorm_agent']
-                if self.use_cuda:
-                    gt_traj=gt_traj.cuda()
-
                 kl_loss = -0.5 * torch.mean(1 + latent_logvar - latent_mean.pow(2) - latent_logvar.exp())
                 mse_loss=self.loss_fn(pred_traj,gt_traj)
                 loss = kl_loss + mse_loss
             else:
                 pred_traj=self.model(traj_dict)
                 pred_traj=self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-                gt_traj=traj_dict['gt_unnorm_agent']
-                if self.use_cuda:
-                    gt_traj=gt_traj.cuda()
                 loss=self.loss_fn(pred_traj,gt_traj)
-                
-            loss=self.loss_fn(pred_traj,gt_traj)
+
+            
             total_loss=total_loss+loss.data
             batch_samples=gt_traj.shape[0]           
             
@@ -264,20 +239,20 @@ class Trainer():
             print(f"Validation Iter {i_batch+1}/{num_batches} Avg Loss {total_loss/(i_batch+1):.4f} \
             One sec:- ADE:{ade_one_sec/(no_samples):.4f} FDE: {fde_one_sec/(no_samples):.4f}\
             Three sec:- ADE:{ade_three_sec/(no_samples):.4f} FDE: {fde_three_sec/(no_samples):.4f}",end="\r")
-        print()
+
         self.save_top_errors_accuracy(model_dir)
-        return total_loss/(num_batches), ade_one_sec/no_samples,fde_one_sec/no_samples,ade_three_sec/no_samples,fde_three_sec/no_samples
+        print("Saved error plots")
 
     def test_model(self,model_dir):
         num_batches=len(self.test_loader.batch_sampler)
         batch_size=self.test_loader.batch_size
-        self.test_model.load_state_dict(torch.load(model_dir+'best-model.pt')['model_state_dict'])
-        self.test_model.eval()
+        self.model.load_state_dict(torch.load(model_dir+'best-model.pt')['model_state_dict'])
+        self.model.eval()
         no_samples=0
         output_all = {}
         for i_batch,traj_dict in enumerate(self.test_loader):
             seq_index=traj_dict['seq_index']
-            pred_traj=self.test_model(traj_dict)
+            pred_traj=self.model(traj_dict)
             pred_traj=self.test_loader.dataset.inverse_transform(pred_traj,traj_dict)
             if self.use_cuda:
                 pred_traj=pred_traj.cpu()
@@ -289,61 +264,117 @@ class Trainer():
         self.save_top_errors_accuracy(model_dir)
 
     def save_top_errors_accuracy(self,model_dir):
-        self.test_model.load_state_dict(torch.load(model_dir+'best-model.pt')['model_state_dict'])
+        self.model.load_state_dict(torch.load(model_dir+'best-model.pt')['model_state_dict'])
+        self.model.eval()
         min_loss=np.inf
         max_loss=0
-        loss_list=[]
-        for i_batch,traj_dict in enumerate(self.val_metric_loader):
-            pred_traj=self.model(traj_dict)
-            # pred_traj=self.val_metric_loader.dataset.inverse_transform(pred_traj,traj_dict)
-            pred_traj=self.val_loader.dataset.inverse_transform(pred_traj,traj_dict)
-            gt_traj=traj_dict['gt_unnorm_agent']
-            if self.use_cuda:
-                gt_traj=gt_traj.cuda()
-            loss=torch.norm(pred_traj.reshape(pred_traj.shape[0],-1),gt_traj.reshape(gt_traj.shape[0],-1),dim=1)
-            min_index,min_loss=torch.min(loss,dim=0)
-            max_index,max_loss=torch.max(loss,dim=0)
-
-            input_=self.val_loader.dataset.inverse_transform(traj_dict['train_agent'],min_traj_dict)
-            # output=self.val_loader.dataset.inverse_transform(self.model(traj_dict),min_traj_dict)
-            output=pred_traj[min_index,:,:]
-            target=traj_dict['gt_unnorm_agent']
-            city_name_min=min_traj_dict['city']
-            # loss_list=loss
-            # if loss<min_loss:
-            #     min_traj_dict=traj_dict
-            #     min_loss=loss
-            # if loss>max_loss:
-            #     max_traj_dict=traj_dict
-            #     max_loss=loss
+        num_images=10
         
+        loss_list_max=[]
+        input_max_list=[]
+        pred_max_list=[]
+        target_max_list=[]
+        city_name_max=[]
+        seq_path_list_max=[]
+        
+        loss_list_min=[]
+        input_min_list=[]
+        pred_min_list=[]
+        target_min_list=[]
+        city_name_min=[]
+        seq_path_list_min=[]
+
+        num_batches=len(self.val_loader.batch_sampler)
+        for i_batch,traj_dict in enumerate(self.val_loader):
+            print(f"Running {i_batch}/{num_batches}",end="\r")
+            gt_traj=traj_dict['gt_unnorm_agent']
+            train_traj=traj_dict['train_agent']
+            if self.use_cuda:
+                train_traj=train_traj.cuda()
+            input_=self.val_loader.dataset.inverse_transform(train_traj,traj_dict)
+            output=self.val_loader.dataset.inverse_transform(self.model(traj_dict),traj_dict)
+            if self.use_cuda:
+                output=output.cpu()
+                input_=input_.cpu()
+            # import pdb;pdb.set_trace()
+            loss=torch.norm(output.reshape(output.shape[0],-1)-gt_traj.reshape(gt_traj.shape[0],-1),dim=1)
+            min_loss,min_index=torch.min(loss,dim=0)
+            max_loss,max_index=torch.max(loss,dim=0)
+
+            
+            input_min_list.append(input_[min_index])
+            pred_min_list.append(output[min_index])
+            target_min_list.append(gt_traj[min_index])
+
+            input_max_list.append(input_[max_index])
+            pred_max_list.append(output[max_index])
+            target_max_list.append(gt_traj[max_index])
+            
+            city_name_min.append(traj_dict['city'][min_index])
+            city_name_max.append(traj_dict['city'][max_index])
+
+            seq_path_list_max.append(traj_dict['seq_path'][max_index])
+            seq_path_list_min.append(traj_dict['seq_path'][min_index])
+
+            loss_list_max.append(min_loss.data)
+            loss_list_min.append(max_loss.data)
+           
+        
+        loss_list_max_array=np.array(loss_list_max)
+        loss_list_max=list(loss_list_max_array.argsort()[-num_images:][::-1])
+
+        loss_list_min_array=np.array(loss_list_min)
+        loss_list_min=list(loss_list_min_array.argsort()[:num_images])
+
         avm=ArgoverseMap()
         
         high_error_path=model_dir+"/visualization/high_errors/"
         low_error_path=model_dir+"/visualization/low_errors/"
 
         if not os.path.exists(high_error_path):
-            os.make_dirs(high_error_path)
+            os.makedirs(high_error_path)
 
         if not os.path.exists(low_error_path):
-            os.make_dirs(low_error_path)
+            os.makedirs(low_error_path)
 
-        if self.use_cuda:
-            input_=input_.cpu()
-            output=output.cpu()
-        centerlines=avm.get_candidate_centerlines_for_traj(input_, city_name_min,viz=False)
-        viz_predictions(input_=input_, output=output,target=target,centerlines=centerlines,city_names=city_name_min,avm=avm,save_path=low_error_path+"/low_err.jpg")
+        input_max=[]
+        pred_max=[]
+        target_max=[]
+        city_max=[]
 
-        input_=self.val_metric_loader.dataset.inverse_transform(max_traj_dict['train_agent'],max_traj_dict)
-        output=self.val_metric_loader.dataset.inverse_transform(self.model(max_traj_dict),max_traj_dict)
-        target=max_traj_dict['gt_unnorm_agent']
-        city_name_max=max_traj_dict['city']
+        centerlines_max=[]
+        for i,index in enumerate(loss_list_max):
+            print(f"Max: {i}")
+            input_max.append(input_max_list[index].detach().numpy())
+            pred_max.append([pred_max_list[index].detach().numpy()])
+            target_max.append(target_max_list[index].detach().numpy())
+            city_max.append(city_name_max[index])
+            viz_sequence(df=pd.read_csv(seq_path_list_max[index]) ,save_path=f"{high_error_path}/dataframe_{i}.png",show=True,avm=avm)
+            centerlines_max.append(avm.get_candidate_centerlines_for_traj(input_max[-1], city_max[-1],viz=False))
+        print("Created max array")
+        
+        input_min=[]
+        pred_min=[]
+        target_min=[]
+        city_min=[]
+        centerlines_min=[]
+        for i,index in enumerate(loss_list_min):
+            print(f"Min: {i}")
+            input_min.append(input_min_list[index].detach().numpy())
+            pred_min.append([pred_min_list[index].detach().numpy()])
+            target_min.append(target_min_list[index].detach().numpy())
+            city_min.append(city_name_min[index])
+            # seq_path_min.append(seq_path_list_min[index])
+            viz_sequence(df=pd.read_csv(seq_path_list_min[index]) ,save_path=f"{low_error_path}/dataframe_{i}.png",show=True,avm=avm)
+            centerlines_min.append(avm.get_candidate_centerlines_for_traj(input_min[-1], city_min[-1],viz=False))
+        print("Created min array")
 
-        if self.use_cuda:
-            input_=input_.cpu()
-            output=output.cpu()
-        centerlines=avm.get_candidate_centerlines_for_traj(input_, current_loader.city,viz=False)
-        viz_predictions(input_=input_, output=output,target=target,centerlines=centerlines,city_names=city_name_max,avm=avm,save_path=high_error_path+"/high_err.jpg")
+        print(f"Saving max visualizations at {high_error_path}")
+        viz_predictions(input_=np.array(input_max), output=pred_max,target=np.array(target_max),centerlines=centerlines_max,city_names=np.array(city_max),avm=avm,save_path=high_error_path)
+        
+        print(f"Saving min visualizations at {low_error_path}")
+        viz_predictions(input_=np.array(input_min), output=pred_min,target=np.array(target_min),centerlines=centerlines_min,city_names=np.array(city_min),avm=avm,save_path=low_error_path)
+
 
     def run(self):
         if args.mode=="train":
@@ -351,8 +382,8 @@ class Trainer():
                 print(f"\nEpoch {epoch}: ")
                 avg_loss_train=self.train_epoch()
                 avg_loss_val,ade_one_sec,fde_one_sec,ade_three_sec,fde_three_sec = self.val_epoch(epoch)
-                if (epoch+1==self.num_epochs):
-                    self.test_epoch()
+                # if (epoch+1==self.num_epochs):
+                    # self.test_epoch()
                     #self.test_model(self.model_dir)
                 # self.writer.scalar_summary('Val/1ADE_Epoch', ade_one_sec, epoch)
                 # self.writer.scalar_summary('Val/3ADE_Epoch', ade_three_sec, epoch)
@@ -360,7 +391,7 @@ class Trainer():
                 # self.writer.scalar_summary('Val/3FDE_Epoch', fde_three_sec, epoch)
         elif args.mode=="validate":
             self.validate_model(self.model_dir)
-        #elif args.mode=="test":
+        # elif args.mode=="test":
             #self.test_model(self.model_dir)
 
 if __name__ == "__main__":
